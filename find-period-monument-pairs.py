d4ac0207e54cfdb6e23b86ce9797f0026466c9da
Ceri Binding <ceri.binding@southwales.ac.uk>
2023-11-02 16:10:13 +0000

    finding period-monument pairs using noun chunks

--- find-period-monument-pairs.py
+++ find-period-monument-pairs.py
@@ -1,6 +1,7 @@
 # temp experimentation - identifying 'paired' entities
 # e.g. medieval furrow, iron age barrow etc.
 from datetime import datetime as DT     # For measuring elapsed time
+import itertools  # for product
 import pandas as pd
 import spacy
 from spacy import displacy
@@ -8,7 +9,7 @@ from rematch2 import NamedPeriodRuler
 from rematch2.VocabularyRuler import create_fish_monument_types_ruler
 
 
-def main(periodo_authority_id: str="", input_text: str="") -> None:
+def main(periodo_authority_id: str = "", input_text: str = "") -> None:
 
     # use a predefined spaCy pipeline, disabling the default NER component
     nlp = spacy.load("en_core_web_sm", disable=['ner'])
@@ -27,50 +28,63 @@ def main(periodo_authority_id: str="", input_text: str="") -> None:
     doc = nlp(cleaned)
 
     # print(doc.ents)
-    #for ent in doc.ents:
-        # if (ent[0].ent_type_ == "NAMEDPERIOD" and ent[0].head.ent_type_ == "MONUMENT"):
-        #print(
-            #f"{ent.start} {ent.text} {ent[0].ent_type_} -> (head: {ent[0].head.i} {ent[0].head} {ent[0].head.ent_type_})")
+    # for ent in doc.ents:
+    # if (ent[0].ent_type_ == "NAMEDPERIOD" and ent[0].head.ent_type_ == "MONUMENT"):
+    # print(
+    # f"{ent.start} {ent.text} {ent[0].ent_type_} -> (head: {ent[0].head.i} {ent[0].head} {ent[0].head.ent_type_})")
 
-    data = [{
-        #"from": ent.start_char,
-        #"to": ent.end_char - 1,
-        #"id": ent.ent_id_,
-        "text": chunk.text,
-        "root": chunk.root.text,
-        #"head": chunk.root.head.text
-    } for chunk in doc.noun_chunks]
-    #for chunk in doc.noun_chunks:
-        #print(chunk.text, chunk.root.text, chunk.root.dep_,
-            #chunk.root.head.text)
-
-
-    print("noun chunks:")
+    # for chunk in doc.noun_chunks:
+    # print(chunk.text, chunk.root.text, chunk.root.dep_,
+    # chunk.root.head.text)
+    # load ents into a DataFrame object:
     pd.set_option('display.max_rows', None)
-    df = pd.DataFrame(data)
-    print(df)
-
-    print("entities:")
-    data = [{
+    df = pd.DataFrame([{
         "from": ent.start_char,
         "to": ent.end_char - 1,
         "id": ent.ent_id_,
         "text": ent.text,
         "type": ent.label_
-    } for ent in doc.ents]
-
-    # load results into a DataFrame object:
-    df = pd.DataFrame(data)
+    } for ent in doc.ents])
+    print("entities:")
     print(df)
+
+    # load noun chunks into a DataFrame object
+    pd.set_option('display.max_rows', None)
+    df = pd.DataFrame([{
+        "from": chunk.start_char,
+        "to": chunk.end_char - 1,
+        "id": chunk.ent_id_,
+        "text": chunk.text,
+        "ents": chunk.ents
+    } for chunk in doc.noun_chunks])
+    print("noun chunks:")
+    print(df)
+
+
+    # looking for PERIOD - OBJECT pairs within noun chunks
+    for chunk in doc.noun_chunks:
+        # identify noun chunks containing _both_ OBJECT and NAMEDPERIOD entities
+        if (any(ent.label_ == "NAMEDPERIOD" for ent in chunk.ents) and 
+            any(ent.label_ == "OBJECT" for ent in chunk.ents)):
+            # get all PERIOD entities in the noun chunk
+            periods = filter(lambda ent: ent.label_ ==
+                             "NAMEDPERIOD", chunk.ents)
+            # get all OBJECT entities in the noun chunk
+            objects = filter(lambda ent: ent.label_ == "OBJECT", chunk.ents)
+            # Use cartesian product to give all PERIOD - OBJECT pairs
+            print(f"\nPERIOD -> OBJECT noun chunk: {chunk.ents}:")
+            for ent1, ent2 in itertools.product(periods, objects):
+                print(f"[{ent1.ent_id_}] '{ent1}' -> '{ent2}' [{ent2.ent_id_}]") 
 
 
 if __name__ == '__main__':
     # write header information to screen
     dt_start = DT.now()
     print(f"{__file__} started at {dt_start.strftime('%Y-%m-%dT%H:%M:%S')}")
-    
+
     # example test text from https://doi.org/10.5284/1100093
-    input_text = """This collection comprises site data(reports, images, spreadsheets, GIS data and site records) from two phases of archaeological evaluation undertaken by Oxford Archaeology in June 2018 (SAWR18) and February 2021 (SAWR21) at West Road, Sawbridgeworth, Hertfordshire. SAWR18 In June 2018, Oxford Archaeology were commissioned by Taylor Wimpey to undertake an archaeological evaluation on the site of a proposed housing development to the north of West Road, Sawbridgeworth(TL 47842 15448). A programme of 19 trenches was undertaken to ground truth the results of a geophysical survey and to assess the archaeological potential of the site. The evaluation confirmed the presence of archaeological remains in areas identified on the geophysics. Parts of a NW-SE‐aligned trackway were found in Trenches 1 and 2. Field boundaries identified by geophysics(also present on the 1839 tithe map) were found in Trenches 5 and 7, towards the south of the site, and in Trenches 12 and 16, in the centre of the site. Geophysical anomalies identified in the northern part of the site were investigated and identified as geological. The archaeology is consistent with the geophysical survey results and it is likely that much of it has been truncated by modern agricultural activity. SAWR21 Oxford Archaeology carried out an archaeological evaluation on the site of proposed residential development north of West Road, Sawbridgeworth, Hertfordshire, in February 2021. The fieldwork was commissioned by Taylor Wimpey as a condition of planning permission. Preceding geophysical survey of the c 5.7ha development site was undertaken in 2016 and identified a concentration of linear and curvilinear anomalies in the north-east corner of the site and two areas of several broadly NW-SE aligned anomalies in the southern half of the site. Subsequent trial trench evaluation, comprising the investigation of 19 trenches, was undertaken by Oxford Archaeology in 2018, targeted upon the geophysical survey results. The evaluation revealed a small number of ditches in the centre and south of the site, correlating with the geophysical anomalies. Although generally undated, the ditches were suggestive of a trackway and associated enclosure/field boundaries. Other ditches encountered on site correlated with post-medieval field boundaries depicted on 19th century mapping. Given the results of the 2018 evaluation, in conjunction with those of the 2018 investigations at nearby Chalk's Farm, which uncovered the remains of late Bronze Age-early Iron Age and early Roman settlement and agricultural activity, it was deemed necessary to undertake a further phase of evaluation at the site. Four additional trenches were excavated in the southern half of the site to further investigate the previously revealed ditches. The continuations of the trackway ditches were revealed in the centre of the site, with remnants of a metalled surface also identified. Adjacent ditches may demonstrate the maintenance and modification of the trackway or perhaps associated enclosure/field boundaries. Artefactual dating evidence recovered from these ditches was limited and of mixed date, comprising small pottery sherds of late Bronze Age-early Iron Age date and fragments of Roman ceramic building material. It is probable that these remains provide evidence of outlying agricultural activity associated with the later prehistoric and early Roman settlement evidence at Chalk's Farm. A further undated ditch and a parallel early Roman ditch were revealed in the south of the site, suggestive of additional land divisions, probably agricultural features. A post-medieval field boundary ditch and modern land drains are demonstrative of agricultural use of the landscape during these periods."""
+    input_text = """This collection comprises site data(reports, images, spreadsheets, GIS data and site records) from two phases of archaeological evaluation undertaken by Oxford Archaeology in June 2018 (SAWR18) and February 2021 (SAWR21) at West Road, Sawbridgeworth, Hertfordshire. SAWR18 In June 2018, Oxford Archaeology were commissioned by Taylor Wimpey to undertake an archaeological evaluation on the site of a proposed housing development to the north of West Road, Sawbridgeworth(TL 47842 15448). A programme of 19 trenches was undertaken to ground truth the results of a geophysical survey and to assess the archaeological potential of the site. The evaluation confirmed the presence of archaeological remains in areas identified on the geophysics. Parts of a NW-SE‐aligned trackway were found in Trenches 1 and 2. Field boundaries identified by geophysics(also present on the 1839 tithe map) were found in Trenches 5 and 7, towards the south of the site, and in Trenches 12 and 16, in the centre of the site. Geophysical anomalies identified in the northern part of the site were investigated and identified as geological. The archaeology is consistent with the geophysical survey results and it is likely that much of it has been truncated by modern agricultural activity. SAWR21 Oxford Archaeology carried out an archaeological evaluation on the site of proposed residential development north of West Road, Sawbridgeworth, Hertfordshire, in February 2021. The fieldwork was commissioned by Taylor Wimpey as a condition of planning permission. Preceding geophysical survey of the c 5.7ha development site was undertaken in 2016 and identified a concentration of linear and curvilinear anomalies in the north-east corner of the site and two areas of several broadly NW-SE aligned anomalies in the southern half of the site. Subsequent trial trench evaluation, comprising the investigation of 19 trenches, was undertaken by Oxford Archaeology in 2018, targeted upon the geophysical survey results. The evaluation revealed a small number of ditches in the centre and south of the site, correlating with the geophysical anomalies. Although generally undated, the ditches were suggestive of a trackway and associated enclosure/field boundaries. Other ditches encountered on site correlated with post-medieval field boundaries depicted on 19th century mapping. Given the results of the 2018 evaluation, in conjunction with those of the 2018 investigations at nearby Chalk's Farm, which uncovered the remains of Late Bronze Age early Iron Age and early Roman settlement and agricultural activity, it was deemed necessary to undertake a further phase of 
+    evaluation at the site. Four additional trenches were excavated in the southern half of the site to further investigate the previously revealed ditches. The continuations of the trackway ditches were revealed in the centre of the site, with remnants of a metalled surface also identified. Adjacent ditches may demonstrate the maintenance and modification of the trackway or perhaps associated enclosure/field boundaries. Artefactual dating evidence recovered from these ditches was limited and of mixed date, comprising small pottery sherds of late Bronze Age-early Iron Age date and fragments of Roman ceramic building material. It is probable that these remains provide evidence of outlying agricultural activity associated with the later prehistoric and early Roman settlement evidence at Chalk's Farm. A further undated ditch and a parallel early Roman ditch were revealed in the south of the site, suggestive of additional land divisions, probably agricultural features. A post-medieval field boundary ditch and modern land drains are demonstrative of agricultural use of the landscape during these periods."""
 
     main(periodo_authority_id="p0kh9ds", input_text=input_text)
 
--- rematch2/VocabularyRuler.py
+++ rematch2/VocabularyRuler.py
@@ -38,6 +38,7 @@ else:
     # uses current package visibility
     from . VocabularyEnum import VocabularyEnum
 
+
 class VocabularyRuler(EntityRuler):
 
     def __init__(
@@ -73,7 +74,7 @@ class VocabularyRuler(EntityRuler):
             overwrite_ents=True,
             ent_id_sep="||"
         )'''
-        
+
         # add terms from vocabulary as generated patterns
         # vocabulary: [ { id, term }, { id, term }]
         # patterns: [{id, label, language, term, pattern}, {id, label, language, term, pattern}]
@@ -121,12 +122,11 @@ class VocabularyRuler(EntityRuler):
 
         return vocabulary
 
-
     # (optionally) lemmatize each word in phrase for better chance of free-text match
     # using SAME nlp pipeline for terms being compared and document text being analysed
 
     @staticmethod
-    def _term_to_pattern(nlp: Language, term: str="", lemmatize: bool=False, min_lemmatize_length:int=4, pos: MutableSequence=[]):
+    def _term_to_pattern(nlp: Language, term: str = "", lemmatize: bool = False, min_lemmatize_length: int = 4, pos: MutableSequence = []):
         # normalise whitespace and force lowercase
         # (whitespace could frustrate matching and
         # lemmatization won't work if capitalised)
@@ -165,7 +165,7 @@ class VocabularyRuler(EntityRuler):
 def create_aat_activities_ruler(nlp: Language, name: str = "aat_activities_ruler") -> VocabularyRuler:
     return VocabularyRuler(
         nlp=nlp,
-        name=name,        
+        name=name,
         default_label="ACTIVITY",
         default_language="en",
         vocabulary=VocabularyRuler._vocabulary_from_enum(
@@ -177,7 +177,7 @@ def create_aat_activities_ruler(nlp: Language, name: str = "aat_activities_ruler
 def create_aat_agents_ruler(nlp: Language, name: str = "aat_agents_ruler") -> VocabularyRuler:
     return VocabularyRuler(
         nlp=nlp,
-        name=name,        
+        name=name,
         pos=["NOUN"],
         default_label="AGENT",
         default_language="en",
@@ -190,7 +190,7 @@ def create_aat_agents_ruler(nlp: Language, name: str = "aat_agents_ruler") -> Vo
 def create_aat_associated_concepts_ruler(nlp: Language, name: str = "aat_associated_concepts_ruler") -> VocabularyRuler:
     return VocabularyRuler(
         nlp=nlp,
-        name=name,        
+        name=name,
         default_label="ASSOCIATED_CONCEPT",
         default_language="en",
         vocabulary=VocabularyRuler._vocabulary_from_enum(
@@ -202,7 +202,7 @@ def create_aat_associated_concepts_ruler(nlp: Language, name: str = "aat_associa
 def create_aat_materials_ruler(nlp: Language, name: str = "aat_materials_ruler") -> VocabularyRuler:
     return VocabularyRuler(
         nlp=nlp,
-        name=name,       
+        name=name,
         default_label="MATERIAL",
         default_language="en",
         vocabulary=VocabularyRuler._vocabulary_from_enum(
@@ -214,7 +214,7 @@ def create_aat_materials_ruler(nlp: Language, name: str = "aat_materials_ruler")
 def create_aat_objects_ruler(nlp: Language, name: str = "aat_objects_ruler") -> VocabularyRuler:
     return VocabularyRuler(
         nlp=nlp,
-        name=name,        
+        name=name,
         pos=["NOUN"],
         default_label="OBJECT",
         default_language="en",
@@ -227,7 +227,7 @@ def create_aat_objects_ruler(nlp: Language, name: str = "aat_objects_ruler") -> 
 def create_aat_physical_attributes_ruler(nlp: Language, name: str = "aat_physical_attributes_ruler") -> VocabularyRuler:
     return VocabularyRuler(
         nlp=nlp,
-        name=name,        
+        name=name,
         default_label="PHYSICAL_ATTRIBUTE",
         default_language="en",
         vocabulary=VocabularyRuler._vocabulary_from_enum(
@@ -239,7 +239,7 @@ def create_aat_physical_attributes_ruler(nlp: Language, name: str = "aat_physica
 def create_aat_styleperiods_ruler(nlp: Language, name: str = "aat_styleperiods_ruler") -> VocabularyRuler:
     return VocabularyRuler(
         nlp=nlp,
-        name=name,        
+        name=name,
         default_label="STYLEPERIOD",
         default_language="en",
         vocabulary=VocabularyRuler._vocabulary_from_enum(
@@ -251,7 +251,7 @@ def create_aat_styleperiods_ruler(nlp: Language, name: str = "aat_styleperiods_r
 def create_fish_archobjects_ruler(nlp: Language, name: str = "fish_archobjects_ruler") -> VocabularyRuler:
     return VocabularyRuler(
         nlp=nlp,
-        name=name,        
+        name=name,
         pos=["NOUN"],
         default_label="OBJECT",
         default_language="en",
@@ -264,7 +264,7 @@ def create_fish_archobjects_ruler(nlp: Language, name: str = "fish_archobjects_r
 def create_fish_archsciences_ruler(nlp: Language, name: str = "fish_archsciences_ruler") -> VocabularyRuler:
     return VocabularyRuler(
         nlp=nlp,
-        name=name,        
+        name=name,
         default_label="ARCHSCIENCE",
         default_language="en",
         vocabulary=VocabularyRuler._vocabulary_from_enum(
@@ -276,7 +276,7 @@ def create_fish_archsciences_ruler(nlp: Language, name: str = "fish_archsciences
 def create_fish_building_materials_ruler(nlp: Language, name: str = "fish_building_materials_ruler") -> VocabularyRuler:
     return VocabularyRuler(
         nlp=nlp,
-        name=name,        
+        name=name,
         default_label="MATERIAL",
         default_language="en",
         vocabulary=VocabularyRuler._vocabulary_from_enum(
@@ -288,7 +288,7 @@ def create_fish_building_materials_ruler(nlp: Language, name: str = "fish_buildi
 def create_fish_components_ruler(nlp: Language, name: str = "fish_components_ruler") -> VocabularyRuler:
     return VocabularyRuler(
         nlp=nlp,
-        name=name,        
+        name=name,
         pos=["NOUN"],
         default_label="OBJECT",
         default_language="en",
@@ -301,7 +301,7 @@ def create_fish_components_ruler(nlp: Language, name: str = "fish_components_rul
 def create_fish_event_types_ruler(nlp: Language, name: str = "fish_event_types_ruler") -> VocabularyRuler:
     return VocabularyRuler(
         nlp=nlp,
-        name=name,        
+        name=name,
         default_label="EVENT",
         default_language="en",
         vocabulary=VocabularyRuler._vocabulary_from_enum(
@@ -313,7 +313,7 @@ def create_fish_event_types_ruler(nlp: Language, name: str = "fish_event_types_r
 def create_fish_evidence_ruler(nlp: Language, name: str = "fish_evidence_ruler") -> VocabularyRuler:
     return VocabularyRuler(
         nlp=nlp,
-        name=name,        
+        name=name,
         default_label="EVIDENCE",
         default_language="en",
         vocabulary=VocabularyRuler._vocabulary_from_enum(
@@ -338,7 +338,7 @@ def create_fish_maritime_craft_ruler(nlp: Language, name: str = "fish_maritime_c
 def create_fish_monument_types_ruler(nlp: Language, name: str = "fish_monument_types_ruler") -> VocabularyRuler:
     return VocabularyRuler(
         nlp=nlp,
-        name=name,        
+        name=name,
         pos=["NOUN"],
         default_label="OBJECT",
         default_language="en",
@@ -351,7 +351,7 @@ def create_fish_monument_types_ruler(nlp: Language, name: str = "fish_monument_t
 def create_fish_periods_ruler(nlp: Language, name: str = "fish_periods_ruler") -> VocabularyRuler:
     return VocabularyRuler(
         nlp=nlp,
-        name=name,        
+        name=name,
         default_label="NAMEDPERIOD",
         default_language="en",
         vocabulary=VocabularyRuler._vocabulary_from_enum(
@@ -371,7 +371,7 @@ if __name__ == "__main__":
     # create pipeline and add one or more custom pipeline components
     nlp = spacy.load("en_core_web_sm", disable=['ner'])
     # AAT vocabulary pipeline components
-    #nlp.add_pipe("aat_activities_ruler", last=True)    
+    # nlp.add_pipe("aat_activities_ruler", last=True)
     # nlp.add_pipe("aat_agents_ruler", last=True)
     # nlp.add_pipe("aat_associated_concepts_ruler", last=True)
     nlp.add_pipe("aat_materials_ruler", last=True)
